75d2299774fd76d32654e46bfced70633a4128a3
Trang Doan <trangdtt.15spt@gmail.com>
2018-12-28 15:58:39 +0700

    - Edit file Tictactoe_.cpp

--- Tictactoe_.cpp
+++ Tictactoe_.cpp
@@ -12,7 +12,7 @@ void Board(char Matrix[max][max])
 	system("cls");
 	cout<<"       Tictactoe\n"<<endl;
 
-	cout<<"Player 1 (X)  -  Player 2 (O)"<<endl<<endl;
+	cout<<"Player 1 (O)  -  Player 2 (X)"<<endl<<endl;
 	cout<<endl;
 	cout<<"           "<<Matrix[0][0]<<"  |  "<<Matrix[0][1] << "  |  " << Matrix[0][2]<<endl;
 	cout<<"           " << Matrix[1][0] << "  |  "<<Matrix[1][1]<<"  |  "<<Matrix[1][2]<<endl;
@@ -95,9 +95,9 @@ int getGameStatus(char Matrix[max][max]) {
 }
 
 
-int PlayGame(char Matrix[max][max], char NamePlayer1, char NamePlayer2, string check)
+int PlayGame(char Matrix[max][max], char NamePlayer1[20], char NamePlayer2[20], string check)
 {
-	int player,count;
+	int player=1,count=0;
 	int number, row, column;
 	do
 	{
@@ -108,7 +108,7 @@ int PlayGame(char Matrix[max][max], char NamePlayer1, char NamePlayer2, string c
 				row = number/10-1;//hàng
 				column = number%10-1; //cot
 				
-				if (checkEmpty(Matrix,row,column) == true) Matrix[row][column] = 'X';
+				if (checkEmpty(Matrix,row,column) == true) Matrix[row][column] = 'O';
 				else
 				{
 					do
@@ -120,15 +120,36 @@ int PlayGame(char Matrix[max][max], char NamePlayer1, char NamePlayer2, string c
 					  Matrix[row][column] = 'X';
 				}
 				Board(Matrix);
+				switch (getGameStatus(Matrix))
+				{
+				case 1: cout << "PLAYER 1 WIN!!!" << endl;
+					check = "win";
+					break;
+				case 2: cout << "PLAYER 2 WIN!!!" << endl;
+					check = "win";
+					break;
+				default:
+					break;
+				}	
+			if (check =="playing")
+			{
 				player++;
 				count++;
+			}
+			if (count == 9) {
+			cout << "DRAW GAME!!!"<<endl;
+			check = "draw";
+			break;
+			}
+			
+				
 				break;
 			case 2:
 				cout<<NamePlayer2<<" enter number: "; cin>>number;
 				row = number/10-1;//hàng
 				column = number%10-1; //cot
 				
-				if (checkEmpty(Matrix,row,column)) Matrix[row][column] = 'O';
+				if (checkEmpty(Matrix,row,column)) Matrix[row][column] = 'X';
 				else
 				{
 					do
@@ -140,33 +161,32 @@ int PlayGame(char Matrix[max][max], char NamePlayer1, char NamePlayer2, string c
 					  Matrix[row][column] = 'O';
 				}
 				Board(Matrix);
-				player++;
-				count--;
-				break;	
-			if (count == 9) {
-			cout << "DRAW GAME!!!"<<endl;
-			check = 'draw';
-			break;
-			}
-			switch (getGameStatus(Matrix))
+				switch (getGameStatus(Matrix))
 				{
 				case 1: cout << "PLAYER 1 WIN!!!" << endl;
-					check = 'win';
+					check = "win";
 					break;
 				case 2: cout << "PLAYER 2 WIN!!!" << endl;
-					check = 'win';
+					check = "win";
 					break;
 				default:
 					break;
 				}	
-			}			
-	} while (check == "playing");
+				
+			if (check =="playing"){
+				player--;
+				count++;
+			}	
+				break;	
+			
+					
+	}} while (check == "playing");
 }
 int main()
 {
 	char Matrix[max][max] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
 	int choice;
-	char NamePlayer1, NamePlayer2,a;
+	char NamePlayer1[20], NamePlayer2[20],a;
 	string kt="playing";
 	cout<<"      Welcome To Tic-tac-toe game! Play with your way!"<<endl;
 	cout<<"\nIf you find any problem, please contact dttt@gameloft.com\n"<<endl;
@@ -174,21 +194,24 @@ int main()
 	cout<<"Select your mode \n";
 		cout<<"1. Play game\n";
 		cout<<"2. Exit\n";
-		cout<<"You choice (1/2): "; cin>>choice;	
+		cout<<"You choice (1/2): "; cin>>choice;
+		cout<<"Enter your name Player 1: "; cin>>NamePlayer1;
+				cout<<"\nEntern your name Player 2: "; cin>>NamePlayer2;	
 	do{
 		for (int i=0;i<3;i++)
 		{
 			for (int j=0;j<3;j++)
 			{
-				Matrix[i][j] == ' ';
+				Matrix[i][j] = ' ';
 			}
 		}
+		
 		switch (choice)
 		{
 			case 1:
-				cout<<"Enter your name Player 1: "; cout<<NamePlayer1;
-				cout<<"\nEntern your name Player 2: "; cout<<NamePlayer2;
+				
 				Board(Matrix);
+				
 				PlayGame(Matrix, NamePlayer1, NamePlayer2,kt);
 				break;
 			case 2:
@@ -196,6 +219,6 @@ int main()
 				break;
 		}
 		cout<<"ban co muon tiep tuc k (y/n)"; cin>>a;	
-	} while (a=='y' || a == 'Y');
+	} while ( a=='y' || a == 'Y');
 	getch();
 }
