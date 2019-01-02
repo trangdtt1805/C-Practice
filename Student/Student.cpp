d2279afe7ca2a026463224797d5538b30cb2f22f
Trang Doan <trangdtt.15spt@gmail.com>
2019-01-02 10:34:29 +0700

    +Add file Student.cpp

--- Student/Student.cpp
+++ Student/Student.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
-#include <fstream>
-#include <conio.h>
-#include <string>
+#include<iostream>
+#include<stdio.h>
+#include<bits/stdc++.h>
+#include<conio.h>
+#include <cstdio>
 
 using namespace std;
 
@@ -11,75 +12,42 @@ struct student
 	char name[200];
 	float score;
 };
+typedef student *st;
 
-void input_Student(student &st)
-{
-	cout<<"Enter Id: "; cin>>st.id;
-	cout<<"Enter Name: "; cin>>st.name;
-//	fflush(stdin);
-//	gets(st.name);
-	cout<<"Enter score: "; cin>>st.score;
-}
-void show_Student(student st)
-{
-	cout<<"Id: "<<st.id<<endl;
-	cout<<"Name: "<<st.name;
-//	puts(st.name);
-	cout<<endl;
-	cout<<"Score: "<<st.score<<endl;
-}
+list<student> ListStudent;
 
-struct Node
+void InputStudent()
 {
-	student Data;
-	Node *Next;
-};
-
-struct List
-{
-	Node *Head;
-	Node *Tail;
-};
-
-void create_List(List &l)
-{
-	l.Head = l.Tail = NULL;
-}
-
-Node* CreateNode(student sv)
-{
-	Node *p = new Node;
-	p->Data = sv;
-	p->Next = NULL;
-	return p;
-}
-
-void AddHead(List &l, student sv)
-{
-	Node *p = CreateNode(sv);
-	if(l.Head==NULL)
-		l.Head=l.Tail=p;
-		else
-		{
-			p->Next=l.Head;
-			l.Head=p;
-		}
-}
-
-void XemList(List l)
-{
-	Node *p = l.Head;
-	while(p!=NULL)
+	int n;
+	cout<<"Enter number student: "; cin>>n;
+	for (int i = 1; i<=n;i++)
 	{
-		show_Student(p->Data);
-		cout<<endl;
-		p=p->Next;
+		cout<<"Studen "<<i<<endl;
+		st student1 = new(student);
+		cout<<"Enter ID: "; cin>>student1->id;
+		cout<<"\nEnter name: "; cin>>student1->name;
+		cout<<"\nEnter score: "; cin>>student1->score;
+		
+		
+		ListStudent.push_back(*student1);
 	}
 }
 
-int main()
+void DislayStudent()
 {
-	List l;
+	system("cls");
+	cout<<"ID\tName\tSCORE";
+	list<student> :: iterator pst;
+	for(pst = ListStudent.begin(); pst != ListStudent.end(); ++pst) 
+    {
+		cout<<endl;
+		cout<<pst->id<<"\t"<<pst->name<<"\t"<<pst->score<<endl;
+
+	}
+}
+
+int Menu()
+{
 	int choice;
 	student st;
 	string x;
@@ -97,10 +65,12 @@ int main()
 		switch (choice)
 		{
 			case 1: 
-				input_Student(st);
+				InputStudent();
+				system("cls");
+				Menu();
 				break;
 			case 2:
-				show_Student(st);
+				DislayStudent();
 				break;
 			case 3: 
 				break;
@@ -110,6 +80,14 @@ int main()
 				break;
 		}
 		cout<<"\nTiep tuc (y/n): ";cin>>x;
+		
 	} while(x == "y" || x == "Y");
+}
+
+
+int main()
+{
+	//ListStudent l;
+	Menu();
 	return 0;
 }
