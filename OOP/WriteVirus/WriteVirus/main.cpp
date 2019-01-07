7de624a0f940d875b3e15e4280632bad8e0ce1fd
Trang Doan <trangdtt.15spt@gmail.com>
2019-01-07 23:49:26 +0700

    - Edit main.cpp

--- OOP/WriteVirus/WriteVirus/main.cpp
+++ OOP/WriteVirus/WriteVirus/main.cpp
@@ -1,5 +1,8 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <cstdlib>
+#include <stdio.h>
+#include <time.h>
 #include "Virus.h"
 #include "FluVirus.h"
 #include "DengueVirus.h"
@@ -9,18 +12,33 @@ using namespace std;
 
 int main()
 {
-	
-	Patient *p = new Patient;
-	char t = '1';
-	while ((p->get_m_state() == 1) && t == '1' )
+	Patient p;
+	char t = 0;
+	while (p.get_m_state == '1')
 	{
 		cout << "Take Medicine (0 = NO, 1 = YES) ";
 		cin >> t;
 		if (t == '1')
 		{
-			p->TakeMedicine();
+			int min = 1;
+			int max = 6;
+			int medicine_resistance = min + (rand() % (int)(max - min + 1));
+			cout << "Medicine resistance = " << medicine_resistance << endl;
+
+			if (medicine_resistance >= 30)
+			{
+				cout << "Virus all died: " << endl;
+
+				break;
+			}
+			else
+			{
+				p.TakeMedicine(medicine_resistance);
+			}
 		}
+		else exit(0);
 	}
+	
 
 	system("pause");
 	return 0;
